cc7648e105fcbdb0df164ae4f17963cf8f3e1d81
KevinMacias181667 <181667@utags.edu.mx>
2022-02-21 21:35:28 -0600

    cambio6.0

--- src/components/Recordatorios/Recordatorios.jsx
+++ src/components/Recordatorios/Recordatorios.jsx
@@ -1,12 +1,5 @@
-//import { supabase } from "../../config/supabaseClient";
 import AppBar from '../../components/AppBar';
-//import { Button } from "@mui/material";
-
 export default function Recordatorios() {
-   
-
-  
-
     return (
         
         <div className="form-widget">
